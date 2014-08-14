0e1be89a044305cbf51dfd069b61f09081486ae4
Donald Piret <donald@donaldpiret.com>
2014-08-14 17:16:49 +0800

    Specify commands for the service

--- sonar/recipes/install.rb
+++ sonar/recipes/install.rb
@@ -20,6 +20,10 @@ end
 # Auto start
 service "sonar" do
   supports :status => true, :restart => true
+  stop_command "sh /opt/sonar/bin/#{node['sonar']['os_kernel']}/sonar.sh stop"
+  start_command "sh /opt/sonar/bin/#{node['sonar']['os_kernel']}/sonar.sh start"
+  status_command "sh /opt/sonar/bin/#{node['sonar']['os_kernel']}/sonar.sh status"
+  restart_command "sh /opt/sonar/bin/#{node['sonar']['os_kernel']}/sonar.sh restart"
   action [ :enable ]
 end
 
